586e3367fd4b22b2d37fce65940b3652eb314802
mairess <trabalho.maires@gmail.com>
2024-08-09 06:26:01 -0300

    feat: implement routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <h1>Home e aqui</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/register" element={ <Register /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
